6ac213f0322a01e9bd07b2f5dda193dd4e129733
Rachel Cravit <rachel.cravit@gmail.com>
2016-04-15 16:41:03 -0400

    Game gives player clue if their guess is higher or lower than random number.

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ var randomNumber = Math.floor(Math.random() * 10) + 1;
 var guess = prompt("I am thinking of a number between 1 and 10. Care to guess what it is?");
   if (parseInt(guess) === randomNumber) {
     correctGuess = true;
+  } else if (parseInt(guess) < randomNumber) {
+      var guessMore = prompt("Try again. The number I am thinking of is greater than " + guess);
+        if (parseInt(guessMore) === randomNumber) {
+          correctGuess = true;
+        }
+
   }
+    else if (parseInt(guess) < randomNumber) {
+      var guessLess = prompt("Try again. The number I am thinking of is less than " + guess);
+        if (parseInt(guess) === randomNumber) {
+          correctGuess = true;
+        }
+    }
 
   if (correctGuess) {
     alert("You are correct!");
   } else {
-    alert("Sorry, the correct answer was " + randomNumber + ".");  }
+    alert("Sorry, the correct answer was " + randomNumber + ".");
+  }

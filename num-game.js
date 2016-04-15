c943d5fbb4cb04d92fe953b9cd387c7139231f86
Rachel Cravit <rachel.cravit@gmail.com>
2016-04-15 16:08:23 -0400

    Added random number generator

--- num-game.js
+++ num-game.js
@@ -1,6 +1,10 @@
 /* Welcome to the itty bitty number guessing game!
-Players guess a number between 1-10.
-They receive clues as to whether the number is higher or lower than their guess. */
+Players must guess a random number between 1-10.
+If incorrect, the player will receive a clue as to whether the random number is higher or lower
+than their guess. */
 
+var correctGuess = false;
+
+var randomNumber = Math.floor(Math.random() * 10) + 1;
 
 
